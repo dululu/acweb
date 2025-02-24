7b9997930700233c7abc1d51a8a4bf1560238d3b
dululu <2399029980@qq.com>
2025-02-25 02:08:33 +0800

    q

---
+++ js-example/js/index.js
@@ -0,0 +1,15 @@
+console.log('Hello, world!');
+
+
+let name = 'John';
+let age = 25;
+
+function printName() {
+  console.log("my name is " + name);
+}
+
+export {
+    name,
+    age,
+    printName
+}
